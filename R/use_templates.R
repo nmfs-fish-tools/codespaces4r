e0e477370c780d71d48ba87af8d4262a41665dab
Kathryn Doering <kathryn.doering@noaa.gov>
2022-11-17 21:01:52 +0000

    fix syntax

--- R/use_templates.R
+++ R/use_templates.R
@@ -8,7 +8,7 @@ use_codespaces <- function() {
    "library-scripts/common-debian.sh")
   lapply(filenames, function(x) {
     usethis::use_github_file( 
-      paste0("https://raw.githubusercontent.com/nmfs-fish-tools/codespaces4r/main"
+      paste0("https://raw.githubusercontent.com/nmfs-fish-tools/codespaces4r/main",
         "/inst/templates/r_radian_vscode/", x), 
       save_as = file.path(".devcontainer", x), 
       ignore = TRUE)
