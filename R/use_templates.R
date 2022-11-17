04372aa7a8441721a8bb44fd7dde3a7e159c25ab
Kathryn Doering <kathryn.doering@noaa.gov>
2022-11-17 21:10:17 +0000

    fix function

--- R/use_templates.R
+++ R/use_templates.R
@@ -4,9 +4,15 @@
 #' @export
 #' @example use_codespaces()
 use_codespaces <- function() {
+  if(!dir.exists(".devcontainer")) {
+    dir.create(".devcontainer")
+  } 
+  if(!dir.exists(file.path(".devcontainer", "library-scripts"))) {
+    dir.create(file.path(".devcontainer", "library-scripts"))
+  }
   filenames <- c("devcontainer.json", "Dockerfile",
    "library-scripts/common-debian.sh")
-  lapply(filenames, function(x) {
+  output <- lapply(filenames, function(x) {
     usethis::use_github_file( 
       paste0("https://raw.githubusercontent.com/nmfs-fish-tools/codespaces4r/main",
         "/inst/templates/r_radian_vscode/", x), 
